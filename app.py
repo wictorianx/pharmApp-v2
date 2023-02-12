d7d4863d070a4d34453058038a27a9ed2f64c4b8
wictorianx <88106599+wictorianx@users.noreply.github.com>
2023-02-12 14:30:50 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
 from flask import Flask, render_template, request, url_for, redirect
 from hashlib import sha256
 import sqlite3
+import pandas as pd
+import pharmacy
 
+logged_in = False
+user_id = None
 def encrypt(x):
     return sha256(x.encode('utf-8')).hexdigest()
-
 con = sqlite3.connect("database.db",check_same_thread=False)
 cur = con.cursor()
 
 app = Flask(__name__)
 
-
+drugs=[]
 @app.route('/', methods=['POST','GET'])
 def hello():
     return "Hello, world!"
-@app.route('/accountExists', methods=['POST','GET'])
+@app.route('/prediction/', methods=['POST','GET'])
+def predict():
+    if request.method == 'POST':  
+        f = request.files['file']
+        filename=f.filename.split(".")[0].capitalize()
+        
+        id = cur.execute("SELECT drug_id FROM drugs WHERE drug_name=(?)",(filename,)).fetchone()[0]
+        pharmacy.readExcel(f,id,user_id)
+        print(filename)
+        print(user_id)
+        print(logged_in)
+        
+    return render_template("prediction.html")
+@app.route('/mypharmacy/', methods=['POST','GET'])
+def myPharmacy():
+    if request.method == 'POST':  
+        f = request.files['file']
+        filename=f.filename.split(".")[0].capitalize()
+        id = cur.execute("SELECT drug_id FROM drugs WHERE drug_name=(?)",(filename,)).fetchone()[0]
+        print(id)
+        #pharmacy.readExcel()
+        
+        
+    return render_template('mypharmacy.html',drugs=drugs)
+@app.route('/accountExists/', methods=['POST','GET'])
 def exists():
-    return "An account with this uername already exists"
+    return "An account with this username already exists"
 @app.route('/createAccount/', methods=['POST','GET'])
 def cacc():
     if request.method == 'POST':
@@ -35,13 +62,19 @@ def cacc():
                 cur.execute("INSERT INTO accounts (encrypted_username, encrypted_password) VALUES (?,?)",(encryptedusername,encryptedpassword))
                 con.commit()
                 print("COMMITTED")
-                return redirect(url_for('hello'))
+                return redirect(url_for('myPharmacy'))
 
             
     return render_template('register.html')
+def convertDrugs(drugs):
+    for i in range(len(drugs)):
+        drugs[i]=list(i)
 
 @app.route('/login/', methods=['POST','GET'])
 def login():
+    global drugs
+    global logged_in
+    global user_id
     print(57686755)
     
     if request.method == 'POST':
@@ -53,10 +86,15 @@ def login():
         try:
             saved_password=cur.execute("SELECT encrypted_password FROM accounts WHERE encrypted_username = (?)",(encryptedusername,)).fetchone()[0]
             if saved_password==encryptedpassword:
+                logged_in = True
                 user_id= cur.execute("SELECT user_id FROM accounts WHERE encrypted_username = (?)",(encryptedusername,)).fetchone()[0]
-                print(cur.execute("SELECT * FROM pharmacies WHERE user_id = (?)",(user_id,)).fetchall())
+                drugs= cur.execute("SELECT * FROM pharmacies WHERE user_id = (?)",(user_id,)).fetchall()
+                for i in range(len(drugs)):
+                    drugs[i]=list(drugs[i])
+                print(1)
+                print(drugs)
                 print("Apo")
-                return redirect(url_for('hello'))
+                return redirect(url_for('myPharmacy'))
 
         except:
             pass
